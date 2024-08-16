ed4aa52e34b5c16270ca3656f5d16096aa9cbc58
hggzjx <525967361@qq.com>
2024-08-16 11:49:42 +0800

    readme

--- run_sup_example.sh
+++ run_sup_example.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# In this example, we show how to train SimCSE using multiple GPU cards and PyTorch's distributed data parallel on supervised NLI dataset.
-# Set how many GPUs to use
-
-# NUM_GPU=4
-
-# Randomly set a port number
-# If you encounter "address already used" error, just run again or manually set an available port id.
-# PORT_ID=$(expr $RANDOM + 1000)
-
-# Allow multiple threads
-# export OMP_NUM_THREADS=8
-
-# Use distributed data parallel
-# If you only want to use one card, uncomment the following line and comment the line with "torch.distributed.launch"
-# python train.py \
 python train.py \
     --model_name_or_path pretrained_model/roberta-base \
     --train_file data/nli_for_simcse.csv \
